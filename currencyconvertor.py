4cb137094e81ce8420ea215540157145af715b51
Dharmil Shah <dharmilshah476@gmail.com>
2022-07-16 01:18:33 +0530

    Completed the Currency Convertor Program

--- currencyconvertor.py
+++ currencyconvertor.py
@@ -2,6 +2,7 @@
 # buttons, labels, text boxes, etc. for our GUI Applications.
 # Tkinter is a Python module for creating GUI applications.
 
+from lib2to3.pytree import convert
 from tkinter import *
 class CurrencyConvertor: # Creat Class
     def __init__(self): #Here self is a variable which represents the instance of the object itself. __init__ special method in python which is to be declared in the class constructor.
@@ -24,3 +25,26 @@ class CurrencyConvertor: # Creat Class
 
         self.converted_amount = StringVar()
         lblConvertAmount = Label(window, textvariable=self.converted_amount, font="Helvetica 12 bold",bg="yellow").grid(row=3, column=2, sticky=E)
+
+        # Creating Convert And Clear Buttons. When they are clicked, respective functions are called.
+
+        btnConvertedAmount = Button(window, text="Convert", command=self.ConvertedAmount, font="Helvetica 12 bold", bg="blue", fg="white").grid(row=4, column=2, sticky=E) # Here in self.ConvertedAmount, ConvertedAmount is the function that is called when the Convert button is clicked.
+
+        btnClear = Button(window, text="Clear", command=self.Clear, font="Helvetica 12 bold", bg="red", fg="white").grid(row=4, column=6, padx=25, pady=25,sticky=E) # Here in self.Clear, Clear is the function that is called when the Clear button is clicked.
+
+        window.mainloop() # This is a function that is used to run the application.
+
+        # Functions to do the conversion. Stores input and performs the conversion.
+
+    def ConvertedAmount(self):
+        amt = float(self.conversion_rate.get())
+        converted_amount = float(self.amount_to_convert.get()) * amt
+        self.converted_amount.set(format(converted_amount, '10.2f'))
+
+        # Functions to do the Clear Input.
+    def Clear(self):
+        self.amount_to_convert.set("")
+        self.conversion_rate.set("")
+        self.converted_amount.set("")
+
+CurrencyConvertor()
