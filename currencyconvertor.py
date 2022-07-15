f7eaaa47483e77b4c659102263f5c1f655d57658
Dharmil Shah <dharmilshah476@gmail.com>
2022-07-16 00:18:48 +0530

    Added the  which is used to create a pop up like structure and gave the title and bg color

--- currencyconvertor.py
+++ currencyconvertor.py
@@ -1,4 +1,6 @@
 from tkinter import *
 class CurrencyConvertor:
-    def __init__(self) -> None: #Here self is a variable which represents the instance of the object itself
-        pass
+    def __init__(self): #Here self is a variable which represents the instance of the object itself
+        window = Tk()
+        window.title("Currency Convertor")
+        window.configure(bg="Yellow")
